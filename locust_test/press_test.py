05a90fc15e23192118020695f34adf175da469b6
daine <daine0513@gmail.com>
2020-01-31 13:44:21 +0800

    daine update

--- locust_test/press_test.py
+++ locust_test/press_test.py
@@ -1,26 +1,76 @@
+import time
+import random
+# from socket import socket, AF_INET, SOCK_STREAM
 import socket
-import sys
+from locust import Locust, TaskSet, events, task
 
-class CreateTcpServer():
-    def start_tcp_socket(self, ip, port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self.server_address = (ip, port)
-        print("start listen on ip %s, port %s") % self.server_address
-        self.sock.bind()
+class TcpSocketClient(socket.socket):
+    def __init__(self, af_inet, socket_type):
+        super(TcpSocketClient, self).__init__(af_inet, socket_type)
 
+    def connect(self, addr):
+        start_time = time.time()
         try:
-            self.sock.listen(1)
-        except socket.error as e:
-            print("fail to listen on port %s") % e
-            sys.exit(1)
-        while True:
-            print
-            "waiting for connection"
-            client, addr = self.sock.accept()
-            client.send("{get out}")
-            client.close()
+            super(TcpSocketClient, self).connect(addr)
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="tcpsocket", name="connect", response_time=total_time, exception=e)
+        else:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="tcpsocket", name="connect", response_time=total_time,
+                                        response_length=0)
 
-if __name__ == '__main__':
-    create = CreateTcpServer()
-    create.start_tcp_socket("10.10.10.33", 8998)
+    def send(self, msg):
+        start_time = time.time()
+        try:
+            super(TcpSocketClient, self).send(msg)
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="tcpsocket", name="send", response_time=total_time, exception=e)
+        else:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="tcpsocket", name="send", response_time=total_time,
+                                        response_length=0)
+
+    def recv(self, bufsize):
+        recv_data = ''
+        start_time = time.time()
+        try:
+            recv_data = super(TcpSocketClient, self).recv(bufsize)
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="tcpsocket", name="recv", response_time=total_time, exception=e)
+        else:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="tcpsocket", name="recv", response_time=total_time,
+                                        response_length=0)
+        return recv_data
+class TcpSocketLocust(Locust):
+    """
+    This is the abstract Locust class which should be subclassed. It provides an TCP socket client
+    that can be used to make TCP socket requests that will be tracked in Locust's statistics.
+    """
+    def __init__(self, *args, **kwargs):
+        super(TcpSocketLocust, self).__init__(*args, **kwargs)
+        self.client = TcpSocketClient(socket.AF_INET, socket.SOCK_STREAM)
+        ADDR = (self.host, self.port)
+        self.client.connect(ADDR)
+
+
+class TcpTestUser(TcpSocketLocust):
+    host = "202.153.5.187"
+    port = 1883
+    min_wait = 1000
+    max_wait = 3000
+
+    class task_set(TaskSet):
+        @task
+        def send_data(self):
+            self.client.send(b"\xff\xff\x00\n\xc9\x00\x01\x00\xa6\r")
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    user = TcpTestUser()
+    user.run()

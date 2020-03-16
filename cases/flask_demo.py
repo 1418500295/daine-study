9e8a31174006019f29326681a8c19685a8f3f112
daine <daine0513@gmail.com>
2020-03-16 15:47:58 +0800

    daine update

--- cases/flask_demo.py
+++ cases/flask_demo.py
@@ -2,8 +2,10 @@ from flask import *
 import json
 
 app = Flask(__name__)
+# 处理接口返回无法显示中文的问题
+app.config['JSON_AS_ASCII'] = False
 
-@app.route('/login/',methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == "GET":
         user = request.args.get("user")
@@ -14,8 +16,9 @@ def login():
         password = request.form.get("password")
         return username + "\n" + password
 
-@app.route('/task/',methods=['GET'])
+@app.route('/task',methods=['GET'])
 def get_all_task():
+
     task = {
         "data": {
             "loginName": "admin",
@@ -28,9 +31,28 @@ def get_all_task():
         "success": 1
 
     }
-    return jsonify(task)
+    data = jsonify(task)
+    return data
 
+@app.route("/getMain",methods=["GET"])
+def get_main():
+    name = request.args.get("name")
+    age = request.args.get("age")
+    return "hello"
 
+@app.route("/postMain",methods=["POST"])
+def post_main():
+    sex = request.form["sex"]
+    color = request.form["color"]
+    json_data = {
+        "msg": "成功",
+        "success": 1
+    }
+    # 将结果转换为json格式
+    return jsonify(json_data)
+
+def post_json_data():
+    request
 
 
 if __name__ == '__main__':

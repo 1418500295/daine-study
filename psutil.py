56d4bdf9663d8c719ba866e4ad59c78a812c25fe
1418500295 <45054883+1418500295@users.noreply.github.com>
2024-08-31 18:12:45 +0800

    Update psutil.py

--- psutil.py
+++ psutil.py
@@ -5,13 +5,17 @@ import datetime
 # run_time = 50
 cpu = []
 memory = []
-io = []
+disk = []
 times = []
 # print("当前日期     时间          CPU使用率 内存使用率 磁盘使用率")
 s = time.time()
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import matplotlib.ticker as ticker
+
 
 class Monitor():
-    def interval(self,i):
+    def interval(self, i):
         intervst = None
         if 0 <= i <= 300:
             intervst = 11
@@ -21,7 +25,26 @@ class Monitor():
             intervst = 21
         return intervst
 
-    def statistics(self,i):
+    def paint(self,example_cpu,intervst):
+        plt.rcParams['font.family'] = 'SimHei'
+        fig = plt.figure(figsize=(20, 7))
+        ax = fig.add_subplot(111)
+        plt.xticks(rotation=45, fontsize=13)
+        plt.yticks(fontsize=13)
+        if example_cpu == "cpu":
+            plt.plot(times, cpu, color='red', label=example_cpu)
+        elif example_cpu == '内存':
+            plt.plot(times, memory, color='green', label=example_cpu)
+        elif example_cpu == '磁盘':
+            plt.plot(times, disk, color='orange', label=example_cpu)
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(intervst))
+        plt.gcf().subplots_adjust(bottom=0.2)  # 保证底部显示完全
+        plt.xlabel("时间", fontsize=13)
+        plt.ylabel(f"{example_cpu}使用率 (%)", fontsize=13)
+        plt.legend(loc='upper right')
+        plt.savefig(f"./{example_cpu}.png")
+        plt.close()
+    def statistics(self, i):
         intervst = self.interval(i)
         try:
             while True:
@@ -30,7 +53,7 @@ class Monitor():
                 memory_usage = psutil.virtual_memory().percent
                 memory.append(memory_usage)
                 disk_usage = psutil.disk_usage('/').percent
-                io.append(disk_usage)
+                disk.append(disk_usage)
                 net = psutil.net_io_counters()
                 current_time = datetime.datetime.now()
                 times.append(current_time.strftime("%m-%d") + "\n" + current_time.strftime("%H:%M:%S"))
@@ -44,70 +67,11 @@ class Monitor():
         except Exception as e:
             print(e)
         finally:
-            import matplotlib.pyplot as plt
-            from matplotlib.widgets import Slider
-            import matplotlib.ticker as ticker
-            x = times
-            y1 = cpu
-            y2 = memory
-            y3 = io
-            plt.rcParams['font.family'] = 'SimHei'
-            fig = plt.figure(figsize=(20, 7))
-            ax = fig.add_subplot(111)
-            plt.xticks(rotation=45, fontsize=13)
-            plt.yticks(fontsize=13)
-            plt.plot(x, y1, color='red', label='cpu')
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(intervst))
-            plt.gcf().subplots_adjust(bottom=0.2)  # 保证底部显示完全
-            plt.xlabel("时间", fontsize=13)
-            plt.ylabel("cpu使用率 (%)", fontsize=13)
-            plt.legend(loc='upper right')
-            # plt.grid(linestyle='--')
+            self.paint("cpu",intervst)
+            self.paint("内存",intervst)
+            self.paint("磁盘",intervst)
 
-            # for a, b in zip(x, y1):
-            #     plt.text(a, b+0.1,b, ha='center', va='bottom', fontsize=8)#y_axis_data1加标签数据
-            plt.savefig("./cpu.png")
-            plt.close()
-            # plt.show()
-
-            fig = plt.figure(figsize=(20, 7))
-            ax = fig.add_subplot(111)
-            plt.xticks(rotation=45, fontsize=13)
-            plt.yticks(fontsize=13)
-            plt.plot(x, y2, color='green', label='内存')
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(intervst))
-            plt.gcf().subplots_adjust(bottom=0.2)  # 保证底部显示完全
-            plt.xlabel("时间", fontsize=13)
-            plt.ylabel("内存使用率 (%)", fontsize=13)
-            plt.legend(loc='upper right')
-            # plt.grid(linestyle='--')
-
-            # for a, b in zip(x, y2):
-            #     plt.text(a, b+0.1,b, ha='center', va='bottom', fontsize=8)#y_axis_data1加标签数据
-
-            plt.savefig("./内存.png")
-            plt.close()
-
-            fig = plt.figure(figsize=(20, 7))
-            ax = fig.add_subplot(111)
-            plt.xticks(rotation=45, fontsize=13)
-            plt.yticks(fontsize=13)
-            plt.plot(x, y3, color='blue', label='磁盘')
-            ax.xaxis.set_major_locator(ticker.MaxNLocator(intervst))
-            plt.gcf().subplots_adjust(bottom=0.2)  # 保证底部显示完全
-            plt.xlabel("时间", fontsize=13)
-            plt.ylabel("磁盘使用率 (%)", fontsize=13)
-            plt.legend(loc='upper right')
-            # plt.grid(linestyle='--')
-
-            # for a, b in zip(x, y3):
-            #     plt.text(a, b+0.1,b, ha='center', va='bottom', fontsize=8)#y_axis_data1加标签数据
-
-            plt.savefig("./磁盘.png")
-            plt.close()
 
 if __name__ == '__main__':
     i = input("输入监控时长(秒)\n")
     Monitor().statistics(int(i))
-
-
